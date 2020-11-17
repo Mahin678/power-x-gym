0b6d33d4da59586308eb84c344e2a7c0d917d28f
Mahin678 <mahintazuar678@gmail.com>
2020-11-17 06:19:53 +0600

    check

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
-	return <div className="App">hello</div>;
+	return <div className="App">hello how are you</div>;
 }
 
 export default App;
